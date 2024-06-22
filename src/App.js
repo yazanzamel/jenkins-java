fb5b40863cce0ab2f7a868f8c209b60fd98ff061
yaznzamel <67708266+yazanzamel@users.noreply.github.com>
2024-06-21 20:46:00 -0700

    modified the title

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Learn Jenkins on Udemy - YAZN
         </a>
       </header>
       <p>
